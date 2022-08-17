6c7c264a577bd1b1b6061e4175235a5b4bb5e586
Guilherme Garcia <garciaguig@gmail.com>
2022-08-17 15:51:55 -0300

    Conclusão do Requisito 03 - Teste o componente FavoritePokemons.js

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+describe('Componente FavoritePokemons.js', () => {
+  it('Teste se é exibida a mensagem "No favorite pokemon found"', () => {
+    const noFavPokemons = [];
+    renderWithRouter(<FavoritePokemons pokemons={ noFavPokemons } />);
+
+    const noPokemonFound = screen
+      .getByText(/No favorite pokemon found/i);
+
+    expect(noPokemonFound).toBeInTheDocument();
+  });
+  // it('Teste se são exibidos todos os cards de pokémons favoritados', () => {
+
+  // });
+});

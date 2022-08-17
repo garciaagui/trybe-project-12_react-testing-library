160e96104add8c999c7ceea0fc34142a59f976a3
Guilherme Garcia <garciaguig@gmail.com>
2022-08-17 16:04:08 -0300

    Conclusão do Requisito 04 - Teste o componente NotFound.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('Componente NotFound.js', () => {
+  it('Teste se a página contém um heading com o texto "Page requested not found"', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundHeading = screen.getByRole('heading',
+      { level: 2, name: /Page requested not found/i });
+
+    expect(notFoundHeading).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém a imagem do Pikachu chorando', () => {
+    renderWithRouter(<NotFound />);
+
+    const srcImg = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const notFoundImg = screen
+      .getByRole('img',
+        { name: /Pikachu crying because the page requested was not found/i });
+
+    expect(notFoundImg).toBeInTheDocument();
+    expect(notFoundImg).toHaveAttribute('src', srcImg);
+  });
+});
